4eadba7d0d581890f8334bb8142deae0b68c374c
Sigrid Muller <sigrid.muller.vidal@gmail.com>
2023-03-22 18:47:57 -0300

    W12 Assignment: A Web Component

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "div1") {
+      x.className += " responsive";
+    } else {
+      x.className = "div1";
+    }
+  }
+
+
